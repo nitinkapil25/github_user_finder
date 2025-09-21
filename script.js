d50e7beee82f7f22de726e1e4a2d6a5d0aa90488
Nitin Kapil <nitinkapil2025@gmail.com>
2025-09-21 15:44:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+
+let searchbtn = document.querySelector("#searchBtn")
+let usernameval = document.querySelector("#username")
+let cardui = document.querySelector("#card")
+
+
+function fetchUserProfile(username){
+   return  fetch(`https://api.github.com/users/${username}`)
+   .then(raw => {
+    if(! raw.ok) throw new Error("user not found")
+
+       return  raw.json()
+   })
+}
+
+
+function op(details){
+    console.log(details);
+    
+   let data =  `
+  <div class="flex items-center space-x-6">
+    
+    <!-- Avatar -->
+    <img src="${details.avatar_url}" 
+         alt="octocat"
+         class="w-24 h-24 rounded-full border-2 border-gray-600 shadow-md">
+    
+    <!-- Info -->
+    <div class="flex-1">
+      <h2 class="text-2xl font-bold text-white">${details.name}</h2>
+      <p class="text-gray-400 text-sm mb-2">${details.bio}</p>
+
+      <!-- Company + Location -->
+      <div class="flex items-center space-x-4 text-sm text-gray-400">
+        <span>🏢 ${details.company}</span>
+        <span>📍${details.location}</span>
+      </div>
+
+      <!-- Stats -->
+      <div class="flex space-x-6 mt-4 text-sm text-gray-300">
+        <span>📂 Repos: <span class="font-semibold">${details.public_repos
+}</span></span>
+        <span>👥 Followers: <span class="font-semibold">${details.followers}</span></span>
+        <span>➡️ Following: <span class="font-semibold">${details.following}</span></span>
+      </div>
+
+      <!-- Profile Link -->
+      <a href="https://github.com/${details.login}" target="_blank"
+         class="inline-block mt-5 px-5 py-2 bg-blue-600 hover:bg-blue-500 rounded-lg text-white text-sm font-medium transition shadow-md">
+        🔗 View Profile
+      </a>
+    </div>
+  </div>
+</div>
+      `;
+       card.classList.remove("animate-pulse");
+      card.innerHTML = data
+
+}
+
+// fetchUserProfile("nitinkapil25")
+// .then((data)=>{
+//     console.log(data);
+    
+// })
+
+searchbtn.addEventListener('click', function(){
+    
+    let username = usernameval.value.trim();
+
+    if(username.length > 0){
+        fetchUserProfile(username).then((data)=>{
+           op(data)
+            
+        })
+
+    }
+    else{
+        alert()
+    }
+})
